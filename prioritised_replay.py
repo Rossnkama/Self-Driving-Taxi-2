169e05b5cf851115e6dc0a5e23284f6234d1203d
Ross Nkama <ross.nkama@adacollege.org.uk>
2018-04-10 03:59:03 +0100

    Added init method for prioritised replay

--- prioritised_replay.py
+++ prioritised_replay.py
@@ -2,4 +2,10 @@
 
 
 class PrioritisedExperienceReplay(object):
-    pass
+
+    def __init__(self, capacity):
+
+        # Max no of transition batches we want in our replay memory
+        self.capacity = capacity
+        # The memory holding the max of the last {capacity} no of events
+        self.memory = []

d2bb283fb1d38b83272c2f958ca0b75bc561b5a8
Ross Nkama <ross.nkama@adacollege.org.uk>
2018-04-17 14:43:00 +0100

    Added non-rank based prioritisation of transitions on binary heap

--- main/prioritised_replay.py
+++ main/prioritised_replay.py
@@ -1,18 +1,24 @@
 # Experience replay
+import heapq
+import operator
 
 
 class DQNPrioritisedReplay(object):
 
     def __init__(self, capacity):
-        """ Initialises the sliding window capacity and memory data struct
+        """ Initialises the sliding window capacity and memory data structure
 
             :param capacity: The number of transitions our replay memory can hold
             :type capacity: int
         """
-        # Max no of transition batches we want in our replay memory
+        # Max no of transition batches we want in our replay memory.
         self.capacity = capacity
-        # The memory holding the max of the last {capacity} no of events
+        # The priority queue binary heap holding the last {capacity} no of events.
         self.memory = []
+        # Determines how much prioritisation is used where 0 is a uniform distribution.
+        self.alpha = 0.2
+        # Small positive number to make sure transitions can still be visited after TD = 0
+        self.epsilon = 0.2
 
     def push(self, transition):
         """ Pushes a new markov process transition + TD-Error to the replay memory window
@@ -20,10 +26,12 @@ class DQNPrioritisedReplay(object):
             :param transition: A single markov process transition + TD-Error
             :type transition: list
         """
-        self.memory.append(transition)
+        # Pushing transition and said transition's temporal difference to memory as a tuple...
+        # ([transition], TDE, priority)
+        heapq.heappush(self.memory, (transition[:len(transition)-1], abs(transition[-1]), (abs(transition[-1]) + self.epsilon ** self.alpha)))
         # Keep no of transitions in sliding window constant
         if len(self.memory) > self.capacity:
             del self.memory[0]
 
     def sample(self, batch_size):
-
+        pass
